7538e10b86d29b324cf5d5457d561bea629d69ab
fareska <fares.xb@gmail.com>
2020-11-24 13:57:37 +0200

    need just styling

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -6,14 +6,5 @@ class Renderer{
         const newHtml = template({item: data})
         $("#recipesContainer").empty().append(newHtml)
 
-    //     for (let obje of data){
-    //         let others = {name: obje.name, link: obje.link, pic: obje.pic}
-    //         let list = obje.ingredients
-    //         const newHtml = template(others)
-    //         const listHtml = template({item: list})
-    //         $("#recipesContainer").empty().append(newHtml)
-    //         $("#recipesContainer").append(listHtml)
-
-    //     }
     }
 }
