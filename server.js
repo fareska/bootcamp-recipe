2709bda10c21794d2191213135976657bde4104c
fareska <fares.xb@gmail.com>
2020-11-24 11:33:57 +0200

    Request to the Recipe API

---
+++ server.js
@@ -0,0 +1,59 @@
+
+const express = require(`express`)
+const app = express()
+const path = require(`path`)
+const url = require(`urllib`)
+
+app.use(express.static(path.join(__dirname, `node_modules`)))
+app.use(express.static(path.join(__dirname, `dist`)))
+
+app.get(`/sanity`, function (req, res){
+    console.log("Every thing is OK")
+    res.send("good for now")
+})
+
+let data
+app.get(`/recipes/:ingredient`, function (req, res){
+    const ingredient = req.params.ingredient
+    url.request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, (err, res)=>{
+        data =JSON.parse(res).results
+    })
+    res.send(data)
+})
+
+
+
+
+// const teamToIDs = {
+//     "lakers": "1610612747",
+//     "warriors": "1610612744",
+//     "heat": "1610612748",
+//     "suns": "1610612756"
+// }
+
+// let fullData
+
+// url.request(`http://data.nba.net/10s/prod/v1/2018/players.json`, (err, res)=>{
+//     fullData=JSON.parse(res).league.standard
+// })
+
+// app.get(`/teams/:teamName`, function(req, res){  
+//     const teamId = teamToIDs[req.params.teamName]
+//     let players = []
+//     for (let i in fullData){
+//         if (fullData[i].teamId== teamId && fullData[i].isActive ){
+//             players.push(fullData[i])
+//         }
+//     }
+//     let relevantData = players.map(p =>{return{ 
+//         firstName:p.firstName, lastName:p.lastName, jersey:p.jersey, pos:p.pos
+//     }})
+
+// res.send(relevantData)
+// })
+
+
+const port = 8080
+app.listen(port, function(){
+    console.log(`Running server on port ${port}`)
+})
