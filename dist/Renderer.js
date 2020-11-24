537fa4588122170017155c82d2b3bb528ca17e32
fareska <fares.xb@gmail.com>
2020-11-24 13:47:30 +0200

    added style

--- dist/Renderer.js
+++ dist/Renderer.js
@@ -3,14 +3,17 @@ class Renderer{
     loadInfo(data) {
         const source = $("#recipes-template").html()
         const template = Handlebars.compile(source)        
-        for (let obje of data){
-            let others = {name: obje.name, link: obje.link, pic: obje.pic}
-            let list = obje.ingredients
-            const newHtml = template(others)
-            const listHtml = template({item: list})
-            $("#recipesContainer").empty().append(newHtml)
-            $("#recipesContainer").append(listHtml)
+        const newHtml = template({item: data})
+        $("#recipesContainer").empty().append(newHtml)
 
-        }
+    //     for (let obje of data){
+    //         let others = {name: obje.name, link: obje.link, pic: obje.pic}
+    //         let list = obje.ingredients
+    //         const newHtml = template(others)
+    //         const listHtml = template({item: list})
+    //         $("#recipesContainer").empty().append(newHtml)
+    //         $("#recipesContainer").append(listHtml)
+
+    //     }
     }
 }
